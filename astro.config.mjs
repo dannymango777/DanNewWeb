9f52c8350a2c45da09f9a4b28d140d3204cd937e
Daniel Mangasarian <156043585+dannymango777@users.noreply.github.com>
2026-01-17 00:16:46 -0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.danielmangasarian.com",
+  site: "https://www.ericminassian.com",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
